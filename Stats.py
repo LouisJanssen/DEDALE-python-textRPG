6571ec44654e9a265987a19a925a549fb73a92d3
LouisJanssen <57401065+LouisJanssen@users.noreply.github.com>
2021-01-06 21:04:00 +0100

    Update Stats.py

--- Stats.py
+++ Stats.py
@@ -1,10 +1,10 @@
-PlayerHp = 10
-PlayerAtk = 3
-
-PlayerStr = 5
-PlayerAgi = 5
-PlayerInt = 5
-PlayerCha = 5
-
-PlayerGold = 0
-
+class Player:
+  def __init__(self):
+    self.name = ""
+    self.Hp = 10
+    self.Atk = 3
+    self.Str = 5
+    self.Agi = 5
+    self.Int = 5
+    self.Cha = 5
+    self.Gold = 0
