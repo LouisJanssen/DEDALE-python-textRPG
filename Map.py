4bc77456f4cbb89ad118e27bf27b687b9ef5e029
LouisJanssen <57401065+LouisJanssen@users.noreply.github.com>
2021-01-08 12:04:42 +0100

    map continue

--- Map.py
+++ Map.py
@@ -3,5 +3,192 @@ class PlayerPos:
     self.x = "A"
     self.y = 1
 
+class CaseA1:
+  def __init__(self):
+    self.x = "A"
+    self.y = 1
+    self.active = True
+
+class CaseA2:
+  def __init__(self):
+    self.x = "A"
+    self.y = 2
+    self.active = True
+
+class CaseA3:
+  def __init__(self):
+    self.x = "A"
+    self.y = 3
+    self.active = True
+
+class CaseA4:
+  def __init__(self):
+    self.x = "A"
+    self.y = 4
+    self.active = True
+
+class CaseA5:
+  def __init__(self):
+    self.x = "A"
+    self.y = 5
+    self.active = True
+
+class CaseB1:
+  def __init__(self):
+    self.x = "B"
+    self.y = 1
+    self.active = True
+
+class CaseB2:
+  def __init__(self):
+    self.x = "B"
+    self.y = 2
+    self.active = True
+
+class CaseB3:
+  def __init__(self):
+    self.x = "B"
+    self.y = 3
+    self.active = True
+
+class CaseB4:
+  def __init__(self):
+    self.x = "B"
+    self.y = 4
+    self.active = True
+
+class CaseB5:
+  def __init__(self):
+    self.x = "B"
+    self.y = 5
+    self.active = True
+
+class CaseC1:
+  def __init__(self):
+    self.x = "C"
+    self.y = 1
+    self.active = True
+
+class CaseC2:
+  def __init__(self):
+    self.x = "C"
+    self.y = 2
+    self.active = True
+
+class CaseC3:
+  def __init__(self):
+    self.x = "C"
+    self.y = 3
+    self.active = True
+
+class CaseC4:
+  def __init__(self):
+    self.x = "C"
+    self.y = 4
+    self.active = True
+
+class CaseC5:
+  def __init__(self):
+    self.x = "C"
+    self.y = 5
+    self.active = True
+
+class CaseD1:
+  def __init__(self):
+    self.x = "D"
+    self.y = 1
+    self.active = True
+
+class CaseD2:
+  def __init__(self):
+    self.x = "D"
+    self.y = 2
+    self.active = True
+
+class CaseD3:
+  def __init__(self):
+    self.x = "D"
+    self.y = 3
+    self.active = True
+
+class CaseD4:
+  def __init__(self):
+    self.x = "D"
+    self.y = 4
+    self.active = True
+
+class CaseD5:
+  def __init__(self):
+    self.x = "D"
+    self.y = 5
+    self.active = True
+
+class CaseE1:
+  def __init__(self):
+    self.x = "E"
+    self.y = 1
+    self.active = True
+
+class CaseE2:
+  def __init__(self):
+    self.x = "E"
+    self.y = 2
+    self.active = True
+
+class CaseE3:
+  def __init__(self):
+    self.x = "E"
+    self.y = 3
+    self.active = True
+
+class CaseE4:
+  def __init__(self):
+    self.x = "E"
+    self.y = 4
+    self.active = True
+
+class CaseE5:
+  def __init__(self):
+    self.x = "E"
+    self.y = 5
+    self.active = True
+
+class CaseF1:
+  def __init__(self):
+    self.x = "F"
+    self.y = 1
+    self.active = True
+
+class CaseF2:
+  def __init__(self):
+    self.x = "F"
+    self.y = 2
+    self.active = True
+
+class CaseF3:
+  def __init__(self):
+    self.x = "F"
+    self.y = 3
+    self.active = True
+
+class CaseF4:
+  def __init__(self):
+    self.x = "F"
+    self.y = 4
+    self.active = True
+
+class CaseF5:
+  def __init__(self):
+    self.x = "F"
+    self.y = 5
+    self.active = True
+
 PlayerPos = PlayerPos()
-print(PlayerPos.x, PlayerPos.y)
+CaseF5 = CaseF5()
+
+i = 'F'
+j = 5
+tmp = 'Case' + i + str(j)
+print(tmp)
+#if ('Case'+i+j.x == PlayerPos.x):
+ # print('ok')
